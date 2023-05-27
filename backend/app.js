86861b3c7c41f8d8047b434ffc17f084a277c117
Bogdanova V <zh-vln@yandex.ru>
2023-05-28 01:57:54 +0300

    refactoring3

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,29 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const errorCelebrate = require('celebrate').errors;
+const helmet = require('helmet');
+const cors = require('./middlewares/cors');
 const router = require('./routes/index');
 const { ERROR_INTERNAL_SERVER } = require('./utils/constants');
 const { PORT, MONGODB } = require('./config');
 const handlerErrors = require('./middlewares/handlerErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const limiter = require('./middlewares/rateLimit');
 
 const app = express();
 mongoose.connect(MONGODB);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Ещё чуть-чуть и сервер упадёт');
+  }, 0);
+});
+
+app.use(cors);
 app.use(cookieParser());
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
 app.use(requestLogger); // подключаем логгер запросов
 app.use('/', router);
 app.use(errorLogger); // подключаем логгер ошибок
