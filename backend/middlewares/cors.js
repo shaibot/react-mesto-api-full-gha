fcafd8b8f97122caab4777286cb2e88981ea7037
Bogdanova V <zh-vln@yandex.ru>
2023-05-29 00:48:55 +0300

    refactoring17

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -22,10 +22,12 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  // Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+  // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-
+// завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
