fbe5ad0835d64f9e02fbcfc60227299800f762df
Bogdanova V <zh-vln@yandex.ru>
2023-05-30 12:18:03 +0300

    refactoring21

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,18 +7,21 @@ const Unauthorized = require('../Error/Unauthorized');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
+    required: true,
     validate: {
       validator: (v) => REGEX_URL.test(v),
       message: 'Указана некорректная ссылка',
